139638774f289b82f413f8d05603de5e22dcc80b
Julia-85 <120277347+Julia-85@users.noreply.github.com>
2024-01-06 14:24:44 +0300

    Create ActorBehavior.java

---
+++ Seminar2/task1/ActorBehavior.java
@@ -0,0 +1,13 @@
+package Seminar2.task1;
+
+public interface ActorBehavior {
+
+    void setMakeOrder();
+    void setTakeOrder();
+    void setInTheQueue();
+    boolean isMakeOrder();
+    boolean isTakeOrder();
+    boolean InTheQueue();
+
+
+}
