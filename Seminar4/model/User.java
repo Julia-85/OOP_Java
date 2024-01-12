2ebe5a980d6ae4da0b0568357f34dcee014e6840
Julia-85 <120277347+Julia-85@users.noreply.github.com>
2024-01-12 23:50:05 +0300

    Create User.java

---
+++ Seminar4/model/User.java
@@ -0,0 +1,14 @@
+package Seminar4.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private final Long id;
+    private String fullName;
+    private Integer age;
+    private String phoneNumber;
+
+}
