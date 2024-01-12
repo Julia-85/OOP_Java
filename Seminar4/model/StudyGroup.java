d77ccd303b8f17914a78a918cd1eeb43d9488b5c
Julia-85 <120277347+Julia-85@users.noreply.github.com>
2024-01-12 23:47:58 +0300

    Create StudyGroup.java

---
+++ Seminar4/model/StudyGroup.java
@@ -0,0 +1,21 @@
+package Seminar4.model;
+// Учебная группа
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+//@AllArgsConstructor
+
+public class StudyGroup {
+
+    private Integer teacher;
+    private List<Integer> students;
+
+    public StudyGroup(Integer teacher, List<Integer> students) {
+        this.teacher = teacher;
+        this.students = students;
+    }
+}
